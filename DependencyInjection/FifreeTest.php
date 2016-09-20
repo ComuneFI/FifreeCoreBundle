7bf27a59d7586b57df3f7dfb35e72ad0be6eda6a
d59495 <andrea.manzi@comune.fi.it>
2016-09-20 13:29:49 +0200

    migliorati test

--- DependencyInjection/FifreeTest.php
+++ DependencyInjection/FifreeTest.php
@@ -123,13 +123,9 @@ class FifreeTest extends WebTestCase
         $container = $client->getContainer();
 
         $session = $container->get('session');
-        /**
-         * @var \FOS\UserBundle\Doctrine\UserManager
-         */
+        /* @var $userManager \FOS\UserBundle\Doctrine\UserManager */
         $userManager = $container->get('fos_user.user_manager');
-        /**
-         * @var \FOS\UserBundle\Security\LoginManager
-         */
+        /* @var $loginManager \FOS\UserBundle\Security\LoginManager */
         $loginManager = $container->get('fos_user.security.login_manager');
         $firewallName = $container->getParameter('fos_user.firewall_name');
 
@@ -137,11 +133,11 @@ class FifreeTest extends WebTestCase
         $user = $userManager->findUserBy(array('username' => $username4test));
         $loginManager->loginUser($firewallName, $user);
 
-        // save the login token into the session and put it in a cookie
-        $container->get('session')->set('_security_'.$firewallName, serialize($container->get('security.context')->getToken()));
+        /* save the login token into the session and put it in a cookie */
+        $container->get('session')->set('_security_'.$firewallName, serialize($container->get('security.token_storage')->getToken()));
         $container->get('session')->save();
         $client->getCookieJar()->set(new Cookie($session->getName(), $session->getId()));
-
+        
         return $client;
     }
 
