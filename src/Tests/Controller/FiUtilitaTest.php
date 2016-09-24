0b9f2959ad1bd0b4dc75ab0ca61d905276516fed
Andrea Manzi <manzolo@libero.it>
2016-09-24 20:58:35 +0200

    refactoring

--- src/Tests/Controller/FiUtilitaTest.php
+++ src/Tests/Controller/FiUtilitaTest.php
@@ -19,8 +19,15 @@ class FiUtilitaTest extends KernelTestCase {
     public function testConfrontoStringe() {
         $fiUtilita = new FiUtilita();
         $parms = array("stringaa" => "manzolo", "stringab" => "manzolo", "tolleranza" => 0);
-        $ret = $fiUtilita->percentualiConfrontoStringhe($parms);
-        $this->assertEquals($ret, 100);
+        $retperc = $fiUtilita->percentualiConfrontoStringhe($parms);
+        $this->assertEquals($retperc, 100);
+
+        $retdata = $fiUtilita->data2db("31/12/2016");
+        $this->assertEquals($retdata, "2016-12-31");
+        $retdatainv = $fiUtilita->data2db("31/12/2016", true);
+        $this->assertEquals($retdatainv, "2016-31-12");
+        $retdatasl = $fiUtilita->data2db("31/12/2016", false, true);
+        $this->assertEquals($retdatasl, "20161231");
     }
 
 }
