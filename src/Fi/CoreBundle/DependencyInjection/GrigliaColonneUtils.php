8dd4b4aeea0d35d565194b7c03054e6d7ca8fb8f
Andrea Manzi <manzolo@libero.it>
2016-09-27 22:21:49 +0200

    refactoring Griglia 22

--- src/Fi/CoreBundle/DependencyInjection/GrigliaColonneUtils.php
+++ src/Fi/CoreBundle/DependencyInjection/GrigliaColonneUtils.php
@@ -38,17 +38,14 @@ class GrigliaColonneUtils {
         /* @var $doctrine \Doctrine\ORM\EntityManager */
         $doctrine = $parametri['doctrine'];
 
-//$infocolonne = $doctrine->getClassMetadata($entityName)->getColumnNames();
+        /* $infocolonne = $doctrine->getClassMetadata($entityName)->getColumnNames(); */
         $infocolonne = $doctrine->getMetadataFactory()->getMetadataFor($entityName);
-//$infocolonne = get_object_vars($infocolonne);
+        /* $infocolonne = get_object_vars($infocolonne); */
 
         foreach ($infocolonne->fieldMappings as $colonna) {
-//getFieldMapping
-//$doctrine->getConnection()->getSchemaManager()->
-//$ret = $doctrine->getMetadataFactory()->getMetadataFor($entityName)->;
-//if ($colonna == 'descrizione' ){
+            /* getFieldMapping */
+            /* $ret = $doctrine->getMetadataFactory()->getMetadataFor($entityName)->; */
             $colonne[$colonna['fieldName']] = $colonna;
-//}
 
             /* $colonne[$colonna] = $doctrine->getClassMetadata($entityName)->getTypeOfField($colonna);
               $colonne[$colonna] = $doctrine->getClassMetadata($entityName)->getColumnName($colonna);
@@ -76,74 +73,6 @@ class GrigliaColonneUtils {
         return $ordinecolonne;
     }
 
-    public static function getCampiExtraNormalizzati(&$campiextra) {
-//Se è un array di una dimensione si trasforma in bidimensionale
-        if (count($campiextra) == count($campiextra, \COUNT_RECURSIVE)) {
-            $campoextraarray = $campiextra;
-            $campiextra = array();
-            foreach ($campoextraarray as $campoextranormalize) {
-                if (is_object($campoextranormalize)) {
-                    $campoextranormalize = get_object_vars($campoextranormalize);
-                    $campiextra[] = $campoextranormalize;
-                }
-            }
-        }
-    }
-
-    public static function getCampiExtraTestataPerGriglia($paricevuti, &$indice, &$nomicolonne, &$modellocolonne) {
-        $campiextra = GrigliaParametriUtils::getParametriCampiExtraTestataPerGriglia($paricevuti);
-        if (!isset($campiextra)) {
-            return;
-        }
-        GrigliaColonneUtils::getCampiExtraNormalizzati($campiextra);
-
-        foreach ($campiextra as $chiave => $colonna) {
-            ++$indice;
-            self::getCampiExtraColonneNormalizzate($colonna);
-
-            $nomicolonne[$indice] = self::getCampiExtraNomiColonne($colonna, $chiave);
-
-            $widthcolonna = self::getCampiExtraWidthColonna($colonna);
-
-            $tipocolonna = self::getCampiExtraTipoColonna($colonna);
-            $idcolonna = self::getCampiExtraNomeCampoColonna($colonna, $chiave);
-            $nomecolonna = self::getCampiExtraIdCampoColonna($colonna, $chiave);
-
-            $modellocolonne[$indice] = array(
-                'name' => $nomecolonna,
-                'id' => $idcolonna,
-                'width' => $widthcolonna,
-                'tipocampo' => $tipocolonna,
-                'search' => false);
-        }
-    }
-
-    public static function getCampiExtraIdCampoColonna($colonna, $chiave) {
-        return isset($colonna['nomecampo']) ? $colonna['nomecampo'] : $chiave;
-    }
-
-    public static function getCampiExtraTipoColonna($colonna) {
-        return isset($colonna['tipo']) ? $colonna['tipo'] : $colonna['type'];
-        ;
-    }
-
-    public static function getCampiExtraNomeCampoColonna($colonna, $chiave) {
-        return isset($colonna['nomecampo']) ? $colonna['nomecampo'] : $chiave;
-    }
-
-    public static function getCampiExtraNomiColonne(&$colonna, $chiave) {
-        return isset($colonna['descrizione']) ? $colonna['descrizione'] : GrigliaUtils::to_camel_case(array('str' => $chiave, 'primamaiuscola' => true));
-    }
-
-    public static function getCampiExtraWidthColonna($colonna) {
-        return isset($colonna['lunghezza']) ? $colonna['lunghezza'] : ($colonna['length'] * GrigliaUtils::MOLTIPLICATORELARGHEZZA > GrigliaUtils::LARGHEZZAMASSIMA ? GrigliaUtils::LARGHEZZAMASSIMA : $colonna['length'] * GrigliaUtils::MOLTIPLICATORELARGHEZZA);
-    }
-
-    public static function getCampiExtraColonneNormalizzate(&$colonna) {
-        if (is_object($colonna)) {
-            $colonna = get_object_vars($colonna);
-        }
-    }
 
     public static function getAliasCampi(&$nomicolonne, &$modellocolonne, &$indice, &$chiave, &$colonna, $paricevuti) {
         $alias = GrigliaParametriUtils::getAliasTestataPerGriglia($paricevuti);
