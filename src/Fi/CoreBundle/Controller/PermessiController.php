c303790b04320b7a35f59e78b004f9525f205ead
Andrea Manzi <manzolo@libero.it>
2016-09-29 08:46:34 +0200

    phpcbf

--- src/Fi/CoreBundle/Controller/PermessiController.php
+++ src/Fi/CoreBundle/Controller/PermessiController.php
@@ -26,31 +26,42 @@ class PermessiController extends FiController
         $em = $this->getDoctrine()->getManager();
         $entities = $em->getRepository($nomebundle.':'.$controller)->findAll();
 
-        $dettaglij = array('operatori_id' => array(
-                array('nomecampo' => 'operatori.username', 'lunghezza' => '200', 'descrizione' => 'Username', 'tipo' => 'text'),
-                array('nomecampo' => 'operatori.operatore', 'lunghezza' => '200', 'descrizione' => 'Operatore', 'tipo' => 'text'),
+        $dettaglij = array(
+            'operatori_id' => array(
+                array('nomecampo' => 'operatori.username',
+                    'lunghezza' => '200',
+                    'descrizione' => 'Username',
+                    'tipo' => 'text', ),
+                array('nomecampo' => 'operatori.operatore',
+                    'lunghezza' => '200',
+                    'descrizione' => 'Operatore',
+                    'tipo' => 'text', ),
             ),
             'ruoli_id' => array(
-                array('nomecampo' => 'ruoli.ruolo', 'lunghezza' => '200', 'descrizione' => 'Ruolo', 'tipo' => 'text'), ),
+                array('nomecampo' => 'ruoli.ruolo',
+                    'lunghezza' => '200',
+                    'descrizione' => 'Ruolo',
+                    'tipo' => 'text', ),
+            ),
         );
 
-        $paricevuti = array('doctrine' => $em, 'nomebundle' => $nomebundle, 'nometabella' => $controller, 'dettaglij' => $dettaglij, 'container' => $container);
-
-        $testatagriglia = array();
-        $testatagriglia['multisearch'] = 1;
-        $testatagriglia['showconfig'] = 1;
+        $paricevuti = array(
+            'doctrine' => $em,
+            'nomebundle' => $nomebundle,
+            'nometabella' => $controller,
+            'dettaglij' => $dettaglij,
+            'container' => $container, );
 
         $testatagriglia = Griglia::testataPerGriglia($paricevuti);
 
         $testata = json_encode($testatagriglia);
-
-        return $this->render(
-            $nomebundle.':'.$controller.':index.html.twig', array(
-                    'entities' => $entities,
-                    'nomecontroller' => $controller,
-                    'testata' => $testata,
-            )
+        $twigparms = array(
+            'entities' => $entities,
+            'nomecontroller' => $controller,
+            'testata' => $testata,
         );
+
+        return $this->render($nomebundle.':'.$controller.':index.html.twig', $twigparms);
     }
 
     public function setParametriGriglia($prepar = array())
@@ -65,7 +76,13 @@ class PermessiController extends FiController
         $tabellej['operatori_id'] = array('tabella' => 'operatori', 'campi' => array('username', 'operatore'));
         $tabellej['ruoli_id'] = array('tabella' => 'ruoli', 'campi' => array('ruolo'));
 
-        $paricevuti = array('container' => $this->container, 'nomebundle' => $nomebundle, 'tabellej' => $tabellej, 'nometabella' => $controller, 'escludere' => $escludi);
+        $paricevuti = array(
+            'container' => $this->container,
+            'nomebundle' => $nomebundle,
+            'tabellej' => $tabellej,
+            'nometabella' => $controller,
+            'escludere' => $escludi,
+        );
 
         if ($prepar) {
             $paricevuti = array_merge($paricevuti, $prepar);
