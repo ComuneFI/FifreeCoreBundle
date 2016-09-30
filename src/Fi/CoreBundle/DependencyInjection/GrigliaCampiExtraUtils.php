947d1454a1088c70583b1f3b12c62987107d493b
d59495 <andrea.manzi@comune.fi.it>
2016-09-30 14:19:07 +0200

    phpcbf

--- src/Fi/CoreBundle/DependencyInjection/GrigliaCampiExtraUtils.php
+++ src/Fi/CoreBundle/DependencyInjection/GrigliaCampiExtraUtils.php
@@ -2,9 +2,11 @@
 
 namespace Fi\CoreBundle\DependencyInjection;
 
-class GrigliaCampiExtraUtils {
+class GrigliaCampiExtraUtils
+{
 
-    public static function getCampiExtraNormalizzati(&$campiextra) {
+    public static function getCampiExtraNormalizzati(&$campiextra)
+    {
         /* Se è un array di una dimensione si trasforma in bidimensionale */
         if (count($campiextra) == count($campiextra, \COUNT_RECURSIVE)) {
             $campoextraarray = $campiextra;
@@ -21,7 +23,8 @@ class GrigliaCampiExtraUtils {
         }
     }
 
-    public static function getCampiExtraTestataPerGriglia($paricevuti, &$indice, &$nomicolonne, &$modellocolonne) {
+    public static function getCampiExtraTestataPerGriglia($paricevuti, &$indice, &$nomicolonne, &$modellocolonne)
+    {
         $campiextra = GrigliaParametriUtils::getParametriCampiExtraTestataPerGriglia($paricevuti);
         if (!isset($campiextra)) {
             return;
@@ -49,37 +52,44 @@ class GrigliaCampiExtraUtils {
         }
     }
 
-    public static function getCampiExtraIdCampoColonna($colonna, $chiave) {
+    public static function getCampiExtraIdCampoColonna($colonna, $chiave)
+    {
         return isset($colonna['nomecampo']) ? $colonna['nomecampo'] : $chiave;
     }
 
-    public static function getCampiExtraTipoColonna($colonna) {
+    public static function getCampiExtraTipoColonna($colonna)
+    {
         return isset($colonna['tipo']) ? $colonna['tipo'] : $colonna['type'];
     }
 
-    public static function getCampiExtraNomeCampoColonna($colonna, $chiave) {
+    public static function getCampiExtraNomeCampoColonna($colonna, $chiave)
+    {
         return isset($colonna['nomecampo']) ? $colonna['nomecampo'] : $chiave;
     }
 
-    public static function getCampiExtraNomiColonne(&$colonna, $chiave) {
+    public static function getCampiExtraNomiColonne(&$colonna, $chiave)
+    {
         $parametri = array('str' => $chiave, 'primamaiuscola' => true);
 
         return isset($colonna['descrizione']) ? $colonna['descrizione'] : GrigliaUtils::toCamelCase($parametri);
     }
 
-    public static function getCampiExtraWidthColonna($colonna) {
+    public static function getCampiExtraWidthColonna($colonna)
+    {
         $width = isset($colonna['lunghezza']) ? $colonna['lunghezza'] : GrigliaUtils::LARGHEZZAMASSIMA;
 
         return $width;
     }
 
-    public static function getCampiExtraColonneNormalizzate(&$colonna) {
+    public static function getCampiExtraColonneNormalizzate(&$colonna)
+    {
         if (is_object($colonna)) {
             $colonna = get_object_vars($colonna);
         }
     }
 
-    public static function getCampiExtraDatiPerGriglia(&$campiextra, &$vettoreriga, $doctrine, $entityName, $singolo) {
+    public static function getCampiExtraDatiPerGriglia(&$campiextra, &$vettoreriga, $doctrine, $entityName, $singolo)
+    {
         /* Gestione per passare campi che non sono nella tabella ma metodi del model (o richiamabili tramite magic method get) */
         if (isset($campiextra)) {
             GrigliaCampiExtraUtils::getCampiExtraNormalizzati($campiextra);
@@ -93,5 +103,4 @@ class GrigliaCampiExtraUtils {
             }
         }
     }
-
 }
