37affa3858126509253370159794e613176c3e3d
d59495 <andrea.manzi@comune.fi.it>
2016-09-30 14:13:02 +0200

    fix campi extra

--- src/Fi/CoreBundle/DependencyInjection/GrigliaCampiExtraUtils.php
+++ src/Fi/CoreBundle/DependencyInjection/GrigliaCampiExtraUtils.php
@@ -2,10 +2,9 @@
 
 namespace Fi\CoreBundle\DependencyInjection;
 
-class GrigliaCampiExtraUtils
-{
-    public static function getCampiExtraNormalizzati(&$campiextra)
-    {
+class GrigliaCampiExtraUtils {
+
+    public static function getCampiExtraNormalizzati(&$campiextra) {
         /* Se è un array di una dimensione si trasforma in bidimensionale */
         if (count($campiextra) == count($campiextra, \COUNT_RECURSIVE)) {
             $campoextraarray = $campiextra;
@@ -16,11 +15,13 @@ class GrigliaCampiExtraUtils
                     $campiextra[] = $campoextranormalize;
                 }
             }
+            if (count($campiextra) == 0) {
+                $campiextra = $campoextraarray;
+            }
         }
     }
 
-    public static function getCampiExtraTestataPerGriglia($paricevuti, &$indice, &$nomicolonne, &$modellocolonne)
-    {
+    public static function getCampiExtraTestataPerGriglia($paricevuti, &$indice, &$nomicolonne, &$modellocolonne) {
         $campiextra = GrigliaParametriUtils::getParametriCampiExtraTestataPerGriglia($paricevuti);
         if (!isset($campiextra)) {
             return;
@@ -44,54 +45,47 @@ class GrigliaCampiExtraUtils
                 'id' => $idcolonna,
                 'width' => $widthcolonna,
                 'tipocampo' => $tipocolonna,
-                'search' => false, );
+                'search' => false,);
         }
     }
 
-    public static function getCampiExtraIdCampoColonna($colonna, $chiave)
-    {
+    public static function getCampiExtraIdCampoColonna($colonna, $chiave) {
         return isset($colonna['nomecampo']) ? $colonna['nomecampo'] : $chiave;
     }
 
-    public static function getCampiExtraTipoColonna($colonna)
-    {
+    public static function getCampiExtraTipoColonna($colonna) {
         return isset($colonna['tipo']) ? $colonna['tipo'] : $colonna['type'];
     }
 
-    public static function getCampiExtraNomeCampoColonna($colonna, $chiave)
-    {
+    public static function getCampiExtraNomeCampoColonna($colonna, $chiave) {
         return isset($colonna['nomecampo']) ? $colonna['nomecampo'] : $chiave;
     }
 
-    public static function getCampiExtraNomiColonne(&$colonna, $chiave)
-    {
+    public static function getCampiExtraNomiColonne(&$colonna, $chiave) {
         $parametri = array('str' => $chiave, 'primamaiuscola' => true);
 
         return isset($colonna['descrizione']) ? $colonna['descrizione'] : GrigliaUtils::toCamelCase($parametri);
     }
 
-    public static function getCampiExtraWidthColonna($colonna)
-    {
+    public static function getCampiExtraWidthColonna($colonna) {
         $width = isset($colonna['lunghezza']) ? $colonna['lunghezza'] : GrigliaUtils::LARGHEZZAMASSIMA;
 
         return $width;
     }
 
-    public static function getCampiExtraColonneNormalizzate(&$colonna)
-    {
+    public static function getCampiExtraColonneNormalizzate(&$colonna) {
         if (is_object($colonna)) {
             $colonna = get_object_vars($colonna);
         }
     }
 
-    public static function getCampiExtraDatiPerGriglia(&$campiextra, &$vettoreriga, $doctrine, $entityName, $singolo)
-    {
+    public static function getCampiExtraDatiPerGriglia(&$campiextra, &$vettoreriga, $doctrine, $entityName, $singolo) {
         /* Gestione per passare campi che non sono nella tabella ma metodi del model (o richiamabili tramite magic method get) */
         if (isset($campiextra)) {
             GrigliaCampiExtraUtils::getCampiExtraNormalizzati($campiextra);
             foreach ($campiextra as $vettore) {
                 foreach ($vettore as $singolocampo) {
-                    $campo = 'get'.ucfirst($singolocampo);
+                    $campo = 'get' . ucfirst($singolocampo);
                     /* @var $doctrine \Doctrine\ORM\EntityManager */
                     $objTabella = $doctrine->find($entityName, $singolo['id']);
                     $vettoreriga[] = $objTabella->$campo();
@@ -99,4 +93,5 @@ class GrigliaCampiExtraUtils
             }
         }
     }
+
 }
