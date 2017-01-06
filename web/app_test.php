b53b4dbe68a648424f63991289dfdeef58a01288
Andrea Manzi <manzolo@libero.it>
2017-01-06 17:48:48 +0100

    new core

--- web/app_test.php
+++ web/app_test.php
@@ -1,12 +1,12 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\Debug\Debug;
+use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
 // for more information
-umask(0000);
+//umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
@@ -17,9 +17,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
 }
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__ . '/../app/autoload.php';
-//Debug::enable();
+Debug::enable();
 
-$kernel = new AppKernel('test', false);
+$kernel = new AppKernel('test', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
