9bd3b8d8f36e858fd213dfc164f889894b8f46e3
Andrea Manzi <manzolo@libero.it>
2017-01-11 23:36:03 +0100

    new test

---
+++ tests/Form/RuoliTypeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Fi\CoreBundle\Tests\Form\Type;
+
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class TabelleTypeTest extends TypeTestCase
+{
+
+    public function testSubmitValidData()
+    {
+        $formData = array(
+            'nometabella' => 'nometabella',
+            'nomecampo' => 'nomecampo',
+            'mostraindex' => true,
+            'ordineindex' => 10,
+            'larghezzaindex' => 20,
+            'etichettaindex' => "etichetta",
+            'mostrastampa' => false,
+            'ordinestampa' => 10,
+            'larghezzastampa' => 20,
+            'etichettastampa' => "etichettastampa",
+            'registrastorico' => false,
+            'operatori' => 1
+        );
+
+        $type = "Fi\CoreBundle\Form\TabelleType";
+        $form = $this->factory->create($type, $formData);
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($formData, $form->getData());
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+
+}
