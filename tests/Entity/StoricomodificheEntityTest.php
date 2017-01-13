6dd09c37ba34d92a5d6641b677425468aaab5f28
Andrea Manzi <manzolo@libero.it>
2017-01-13 19:21:37 +0100

    add tests

---
+++ tests/Entity/StoricomodificheEntityTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Fi\CoreBundle\Tests\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class StoricomodificheRepositoryFunctionalTest extends KernelTestCase
+{
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+        $this->em = static::$kernel->getContainer()
+                ->get('doctrine')
+                ->getManager()
+        ;
+    }
+
+    public function testSearchBy()
+    {
+        $object = $this->em
+                ->getRepository('FiCoreBundle:Storicomodifiche')
+                ->findByNometabella('Storicomodifiche')
+        ;
+
+        $this->assertCount(0, $object);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+
+}
