827f2823f7f8b264bbb09baa620899560f9743cf
d59495 <andrea.manzi@comune.fi.it>
2017-11-03 14:14:41 +0100

    migrazione symfony 3.4

--- tests/Command/CreateEnvFifreeTest.php
+++ tests/Command/CreateEnvFifreeTest.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 class CreateEnvFifreeTest extends KernelTestCase
 {
 
-    public function test10InstallFifree()
+    /*public function test10InstallFifree()
     {
 
         $kernel = $this->createKernel();
@@ -28,7 +28,7 @@ class CreateEnvFifreeTest extends KernelTestCase
         );
 
         $this->assertRegExp('/.../', $commandTester->getDisplay());
-    }
+    }*/
 
     public function test15InstallFifree()
     {
@@ -44,7 +44,7 @@ class CreateEnvFifreeTest extends KernelTestCase
         $commandTester->execute(
                 array(
                     '--force' => true,
-                    '--no-interaction' => 'true'
+                    '--no-interaction' => true
                 )
         );
 
@@ -72,6 +72,44 @@ class CreateEnvFifreeTest extends KernelTestCase
 
         $this->assertRegExp('/.../', $commandTester->getDisplay());
     }
+
+    /*public function test30InstallFifree()
+    {
+
+        $kernel = $this->createKernel();
+        $kernel->boot();
+
+        $application = new Application($kernel);
+        $application->add(new \Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand());
+
+        $console = __DIR__ . '/../../bin/console';
+        $cmd = "php " . $console . " generate:bundle --namespace=Fi/ProvaBundle --dir=src/ --format=yml --no-interaction --env=test";
+        echo passthru($cmd);
+
+        $command = $application->find('pannelloamministrazione:generateentities');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+                array(
+                    'mwbfile' => 'wbadmintest.mwb',
+                    'bundlename' => 'Fi/ProvaBundle',
+                    '--schemaupdate' => true,
+                )
+        );
+        echo $commandTester->getDisplay();exit;
+        $this->assertRegExp('/.../', $commandTester->getDisplay());
+
+        $apppath = new \Fi\PannelloAmministrazioneBundle\DependencyInjection\ProjectPath($kernel->getContainer());
+        $checkent = $apppath->getSrcPath() . DIRECTORY_SEPARATOR . "Fi" . DIRECTORY_SEPARATOR . "ProvaBundle" .
+                DIRECTORY_SEPARATOR . "Entity" . DIRECTORY_SEPARATOR . "Prova.php";
+
+
+        $checkres = $apppath->getSrcPath() . DIRECTORY_SEPARATOR . "Fi" . DIRECTORY_SEPARATOR . "ProvaBundle" .
+                DIRECTORY_SEPARATOR . "Resources" . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR .
+                "doctrine" . DIRECTORY_SEPARATOR . "Prova.orm.yml";
+
+        $this->assertTrue(file_exists($checkent));
+        $this->assertTrue(file_exists($checkres));
+    }*/
 
     /*
       $output = $this->runCommand($client, 'fifree2:mysqlconvertdbengine INNODB --tablesfifree2 --env=test --no-interaction');
