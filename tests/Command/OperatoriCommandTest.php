832b59159655d45b66bc77eaee8ef551ba765c18
d59495 <andrea.manzi@comune.fi.it>
2017-11-09 15:19:22 +0100

    migrazione symfony 3.4

--- tests/Command/OperatoriCommandTest.php
+++ tests/Command/OperatoriCommandTest.php
@@ -2,31 +2,71 @@
 
 namespace Fi\CoreBundle\Tests\Command;
 
-class OperatoriCommandTest extends CommandTestCase
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class OperatoriCommandTest extends WebTestCase
 {
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+    }
+
     public function testAddOperatore()
     {
         $userprova = 'utenteprova';
-        $client = self::createClient();
-        $output = $this->runCommand($client, "fos:user:create $userprova $userprova@domain.it passwordprova --inactive");
+        $kernel = static::$kernel;
+        $application = new Application($kernel);
 
-        $em = $client->getKernel()->getContainer()->get('doctrine')->getManager();
+        $application->add(new \FOS\UserBundle\Command\CreateUserCommand());
+        $application->add(new \FOS\UserBundle\Command\ActivateUserCommand());
+
+        $command = $application->find('fos:user:create');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+                array(
+                    'username' => $userprova,
+                    'email' => $userprova . '@domain.it',
+                    'password' => 'passwordprova',
+                    '--inactive' => true
+                )
+        );
+        $output = $commandTester->getDisplay();
+
+        $this->assertRegExp('/.../', $commandTester->getDisplay());
+
+        $em = $kernel->getContainer()->get('doctrine')->getManager();
 
         $user = $em->getRepository('FiCoreBundle:Operatori')->findOneBy(array(
             'username' => $userprova,
         ));
 
         $this->assertContains('Created user', $output);
-        $this->assertEquals($userprova.'@domain.it', $user->getEmail());
+        $this->assertEquals($userprova . '@domain.it', $user->getEmail());
         $this->assertEquals(false, $user->isEnabled());
 
-        $output = $this->runCommand($client, "fos:user:activate $userprova");
+        $command = $application->find('fos:user:activate');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+                array(
+                    'username' => $userprova
+                )
+        );
+        $output = $commandTester->getDisplay();
+        $this->assertRegExp('/.../', $commandTester->getDisplay());
 
         $this->assertContains('has been activated', $output);
-        $this->assertEquals($userprova.'@domain.it', $user->getEmail());
+        $this->assertEquals($userprova . '@domain.it', $user->getEmail());
         $this->assertEquals(true, $user->isEnabled());
 
         $em->remove($user);
         $em->flush();
     }
+
 }
