5dff721410e9833a1edeb1640f1243ecb3ca4a5b
d59495 <andrea.manzi@comune.fi.it>
2017-11-27 12:35:12 +0100

    upgrade symfony 3.4 + php 7.2

--- tests/Controller/GrigliaControllerTest.php
+++ tests/Controller/GrigliaControllerTest.php
@@ -56,9 +56,9 @@ class GrigliaControllerTest extends FifreeTest
         );
 
         /* @var $userManager \FOS\UserBundle\Doctrine\UserManager */
-        $userManager = $container->get('fos_user.user_manager');
+        $userManager = $container->get('fifree.fos_user.user_manager');
         /* @var $loginManager \FOS\UserBundle\Security\LoginManager */
-        $loginManager = $container->get('fos_user.security.login_manager');
+        $loginManager = $container->get('fifree.fos_user.security.login_manager');
         $firewallName = $container->getParameter('fos_user.firewall_name');
         $username4test = $container->getParameter('user4test');
         $user = $userManager->findUserBy(array('username' => $username4test));
