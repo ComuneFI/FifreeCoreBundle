2b2b20f49ccaf9f270a5fb63f927c3ff5b1170a6
d59495 <andrea.manzi@comune.fi.it>
2017-12-14 15:50:27 +0100

    rinforzati header per sicurezza

--- src/Fi/CoreBundle/Listener/ResponseListener.php
+++ src/Fi/CoreBundle/Listener/ResponseListener.php
@@ -24,9 +24,13 @@ class ResponseListener
         $securityProtection = "default-src 'self' http://*.comune.fi.it http://*.comune.intranet https:;"
                 . "script-src 'self' http://*.comune.fi.it http://*.comune.intranet https: 'unsafe-inline' 'unsafe-eval' ;"
                 . "style-src   'self' http://*.comune.fi.it http://*.comune.intranet https: 'unsafe-inline' ;"
-                . "img-src     'self' http://*.comune.fi.it http://*.comune.intranet http://a.tile.openstreetmap.org data: https: ;         object-src  'self' http://*.comune.fi.it http://*.comune.intranet ;         media-src   'self' http://*.comune.fi.it http://*.comune.intranet ;         child-src   'self' http://*.comune.fi.it http://*.comune.intranet https:;"
+                . "img-src     'self' http://*.comune.fi.it http://*.comune.intranet http://a.tile.openstreetmap.org data: https: ;"
+                . "object-src  'self' http://*.comune.fi.it http://*.comune.intranet ;"
+                . "media-src   'self' http://*.comune.fi.it http://*.comune.intranet ;"
+                . "child-src   'self' http://*.comune.fi.it http://*.comune.intranet https:;"
                 . "form-action 'self' http://*.comune.fi.it http://*.comune.intranet https: ;"
                 . "frame-ancestors 'self' http://*.comune.fi.it http://*.comune.intranet https: ;";
+        
         $event->getResponse()->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $event->getResponse()->headers->set('X-Content-Type-Options', 'nosniff');
         $event->getResponse()->headers->set('X-XSS-Protection', '1; mode=block');
