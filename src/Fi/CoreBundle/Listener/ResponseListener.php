258e7a492ca35aeac79854d42b99072a2fa0582a
d59495 <andrea.manzi@comune.fi.it>
2017-12-14 14:00:10 +0100

    aggiunta protezione X-Frame-Options

---
+++ src/Fi/CoreBundle/Listener/ResponseListener.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ResponseListener
+ *
+ * @author d59495
+ */
+
+namespace Fi\CoreBundle\Listener;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class ResponseListener
+{
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $event->getResponse()->headers->set('x-frame-options', 'deny');
+    }
+}
