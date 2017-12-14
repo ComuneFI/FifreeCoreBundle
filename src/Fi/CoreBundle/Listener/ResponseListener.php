11efef84b7efa15bfabfb5d111a2706d7c1b45e7
d59495 <andrea.manzi@comune.fi.it>
2017-12-14 15:20:29 +0100

    rinforzati header per sicurezza

--- src/Fi/CoreBundle/Listener/ResponseListener.php
+++ src/Fi/CoreBundle/Listener/ResponseListener.php
@@ -18,11 +18,18 @@ use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
 class ResponseListener
 {
+
     public function onKernelResponse(FilterResponseEvent $event)
     {
+        $securityProtection = "default-src 'self' http://*.comune.fi.it http://*.comune.intranet https:;"
+                . "script-src 'self' http://*.comune.fi.it http://*.comune.intranet https: 'unsafe-inline' 'unsafe-eval' ;"
+                . "style-src   'self' http://*.comune.fi.it http://*.comune.intranet https: 'unsafe-inline' ;"
+                . "img-src     'self' http://*.comune.fi.it http://*.comune.intranet http://a.tile.openstreetmap.org data: https: ;         object-src  'self' http://*.comune.fi.it http://*.comune.intranet ;         media-src   'self' http://*.comune.fi.it http://*.comune.intranet ;         child-src   'self' http://*.comune.fi.it http://*.comune.intranet https:;"
+                . "form-action 'self' http://*.comune.fi.it http://*.comune.intranet https: ;"
+                . "frame-ancestors 'self' http://*.comune.fi.it http://*.comune.intranet https: ;";
         $event->getResponse()->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $event->getResponse()->headers->set('X-Content-Type-Options', 'nosniff');
         $event->getResponse()->headers->set('X-XSS-Protection', '1; mode=block');
-        $event->getResponse()->headers->set('X-Content-Security-Policy', "default-src 'self' http://*.co…://*.comune.intranet https: ;");
+        $event->getResponse()->headers->set('X-Content-Security-Policy', $securityProtection);
     }
 }
