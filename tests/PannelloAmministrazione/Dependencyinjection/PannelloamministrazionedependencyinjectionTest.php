fce6e4b34dfbe26ddf65afae4a36b36599185979
Andrea Manzi <manzolo@libero.it>
2018-01-09 22:24:57 +0100

    improve test

---
+++ tests/PannelloAmministrazione/Dependencyinjection/PannelloamministrazionedependencyinjectionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Fi\PannelloAmministrazioneBundle\Tests\Dependencyinjection;
+
+use Fi\CoreBundle\DependencyInjection\FifreeTestUtil;
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Fi\PannelloAmministrazioneBundle\DependencyInjection\PannelloAmministrazioneUtils;
+
+class PannelloamministrazionedependencyinjectionTest extends FifreeTestUtil
+{
+
+    public static function setUpBeforeClass()
+    {
+        //cleanFilesystem();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->setClassName(get_class());
+    }
+
+    /*
+     * @test
+     */
+
+    public function test10Dependencyinjection()
+    {
+        $client = $this->getClientAutorizzato();
+        $container = $client->getContainer();
+        $lock = new \Fi\PannelloAmministrazioneBundle\DependencyInjection\LockSystem($container);
+        $this->assertFalse($lock->isLockedFile());
+        $lock->lockFile(true);
+        $this->assertTrue($lock->isLockedFile());
+        $lock->forceCleanLockFile();
+        $this->assertFalse($lock->isLockedFile());
+        $this->assertContains("running.run", $lock->getFileLock());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        cleanFilesystem();
+        clearcache();
+        parent::tearDown();
+    }
+
+}
