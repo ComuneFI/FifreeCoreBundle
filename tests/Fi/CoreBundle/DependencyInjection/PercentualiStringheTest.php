6f2330f3cfd58e367584306011f446c6ccf240a6
d59495 <andrea.manzi@comune.fi.it>
2018-01-16 17:53:08 +0100

    aggiunti tests

---
+++ tests/Fi/CoreBundle/DependencyInjection/PercentualiStringheTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Fi\CoreBundle\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Fi\CoreBundle\DependencyInjection\PercentualiStringhe;
+
+class PercentualiStringheTest extends WebTestCase
+{
+
+    private $parametri = array();
+
+    public function setUp()
+    {
+        $this->parametri[] = array("stringaa" => "manzi", "stringab" => "manzi", "tolleranza" => 0, "percentuale" => 100);
+        $this->parametri[] = array("stringaa" => "manzi", "stringab" => "manzo", "tolleranza" => 0, "percentuale" => 80);
+        $this->parametri[] = array("stringaa" => "manzi", "stringab" => "man", "tolleranza" => 0, "percentuale" => 75);
+        $this->parametri[] = array("stringaa" => "manzi", "stringab" => "m", "tolleranza" => 0, "percentuale" => 33.333);
+        $this->parametri[] = array("stringaa" => "manzi", "stringab" => "cicci", "tolleranza" => 0, "percentuale" => 20);
+        $this->parametri[] = array("stringaa" => "manzi", "stringab" => "cicci", "tolleranza" => 100, "percentuale" => 10);
+    }
+
+    public function testOrderBy()
+    {
+        $prcobjarray = new PercentualiStringhe($this->parametri);
+        $percs = $prcobjarray->percentualiConfrontoStringheVettore($this->parametri);
+        foreach ($this->parametri as $parametro) {
+            $testarray = array("stringaa" => $parametro["stringaa"], "stringab" => $parametro["stringab"], "tolleranza" => $parametro["tolleranza"]);
+            $prcobj = new PercentualiStringhe($testarray);
+            $perc = $prcobj->percentualiConfrontoStringhe($testarray);
+            $this->assertEquals($parametro["percentuale"], round($perc, 3));
+        }
+    }
+
+}
