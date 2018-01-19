ea00eb977d312b620a720c0d89d0e8d2a7d73723
d59495 <andrea.manzi@comune.fi.it>
2018-01-19 10:14:31 +0100

    add tests

--- tests/Fi/CoreBundle/Controller/FfsecondariaControllerTest.php
+++ tests/Fi/CoreBundle/Controller/FfsecondariaControllerTest.php
@@ -13,22 +13,128 @@ class FfsecondariaControllerTest extends FifreeTestAuthorizedClient
      */
     public function testIndexFfsecondaria()
     {
+        $em = $this->getEntityManager();
+
         $client = $this->getClient();
         $url = $client->getContainer()->get('router')->generate('Ffsecondaria');
-        $em = $this->getEntityManager();
         //$this->assertContains('DoctrineORMEntityManager', get_class($em));
 
-        $client->request('GET', $url);
-        $crawler = new Crawler($client->getResponse()->getContent());
+        $crawler = $client->request('GET', $url);
         $this->assertTrue($client->getResponse()->isSuccessful());
         $body = $crawler->filter('div[id="Ffsecondaria"]');
         $attributes = $body->extract(array('_text', 'class'));
         $this->assertEquals($attributes[0][1], 'tabella');
 
-        $clientnoauth = $this->getClient();
-        $urlnoauth = '/Ffsecondaria/';
-        $clientnoauth->request('GET', $urlnoauth);
-
-        $this->assertEquals(200, $clientnoauth->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    /**
+     * @test
+     * @covers Fi\CoreBundle\Controller\FfSecondariaController::<public>
+     * @covers Fi\CoreBundle\Entity\StoricomodificheRepository::<public>
+     */
+    public function testNewFfsecondaria()
+    {
+        $em = $this->getEntityManager();
+
+        $client = $this->getClient();
+
+        //insert
+        $urlnew = $client->getContainer()->get('router')->generate('Ffsecondaria_new');
+        $crawler = $client->request('GET', $urlnew);
+        $this->assertTrue($crawler->filter('html:contains("formdatiFfsecondaria")')->count() > 0);
+        $descrizione = "descsec";
+        /* Inserimento */
+        if (version_compare(\Symfony\Component\HttpKernel\Kernel::VERSION, '3.0') >= 0) {
+            $fieldprefix = 'ffsecondaria';
+        } else {
+            $fieldprefix = 'fi_corebundle_ffsecondariatype';
+        }
+        $valore = "provacrawler";
+        $campodescrizione = $fieldprefix . "[" . $descrizione . "]";
+
+        $valoreffprincipale = "1";
+        $campoffprincipale = $fieldprefix . "[ffprincipale]";
+        $valoredatad = (int)date("d");
+        $valoredatam = (int)date("m");
+        $valoredatay = (int)date("Y");
+        $campodatad = $fieldprefix . "[data][day]";
+        $campodatam = $fieldprefix . "[data][month]";
+        $campodatay = $fieldprefix . "[data][year]";
+
+        $campoattivo = $fieldprefix . "[attivo]";
+        $valoreattivo = 1;
+
+        $campointero = $fieldprefix . "[intero]";
+        $valoreintero = 1000;
+
+        $campoimporto = $fieldprefix . "[importo]";
+        $valoreimporto = 10000;
+
+        $camponota = $fieldprefix . "[nota]";
+        $valorenota = "Notaaaa";
+
+        // submit that form
+        $form = $crawler->filter('form[id=formdatiFfsecondaria]')->form(array
+            ($campodescrizione => $valore,
+            $campoffprincipale => $valoreffprincipale,
+            $campodatad => $valoredatad,
+            $campodatam => $valoredatam,
+            $campodatay => $valoredatay,
+            $campoattivo => $valoreattivo,
+            $campointero => $valoreintero,
+            $campoimporto => $valoreimporto,
+            $camponota => $valorenota,
+        ));
+
+        $crawler = $client->submit($form);
+        //echo $crawler->html();exit;
+
+        //update
+        /* @var $qb \Doctrine\ORM\QueryBuilder */
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select(array('a'));
+        $qb->from('FiCoreBundle:Ffsecondaria', 'a');
+        $qb->where('a.descsec = :descrizione');
+        $qb->setParameter('descrizione', $valore);
+        $record = $qb->getQuery()->getResult();
+        $recordadded = $record[0];
+
+        $this->assertEquals($recordadded->getDescsec(), $valore);
+
+        $crawler = $client->request('GET', '/Ffsecondaria/' . $recordadded->getId() . '/edit');
+        $this->assertTrue($crawler->filter('html:contains("formdatiFfsecondaria")')->count() > 0);
+
+        $valorenew = "provacrawler2";
+        $campodescrizione = $fieldprefix . "[" . $descrizione . "]";
+        $form = $crawler->filter('form[id=formdatiFfsecondaria]')->form(array("$campodescrizione" => $valorenew));
+
+        // submit that form
+        $crawler = $client->submit($form);
+
+
+        $em->clear();
+        $em = $this->getEntityManager();
+        $qb2 = $em->createQueryBuilder();
+        $qb2->select(array('a'));
+        $qb2->from('FiCoreBundle:Ffsecondaria', 'a');
+        $qb2->where('a.descsec = :descrizione');
+        $qb2->setParameter('descrizione', $valorenew);
+        $record2 = $qb2->getQuery()->getResult();
+        $recorddelete = $record2[0];
+        $this->assertEquals($recorddelete->getDescsec(), $valorenew);
+
+        //delete
+        $crawler = $client->request('GET', '/Ffsecondaria/' . $recorddelete->getId() . '/edit');
+        $this->assertTrue($crawler->filter('html:contains("formdatiFfsecondaria")')->count() > 0);
+        $btn = $crawler->selectLink("Elimina")->link();
+        $client->click($btn);
+
+        //Non si riesce a premere Cancella si vede perchè viene creato a runtime il pulsante
+        //quindi lancio la delete a mano
+
+        $crawler = $client->request('GET', '/Ffsecondaria/' . $recorddelete->getId() . '/delete');
+    }
+
 }
