6ec6b11f177abdc61fe77cc26aa03335bcf25822
Andrea Manzi <manzolo@libero.it>
2018-02-11 17:51:41 +0100

    add import export fifree configuration

---
+++ src/Fi/CoreBundle/Command/Fifree2configuratorexportCommand.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Fi\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Filesystem\Filesystem;
+use Doctrine\Common\Persistence\Proxy;
+
+class Fifree2configuratorexportCommand extends ContainerAwareCommand
+{
+
+    private $entities = array();
+
+    protected function configure()
+    {
+        $this
+                ->setName('fifree2:configuratorexport')
+                ->setDescription('Configuratore per Fifree')
+                ->setHelp('Esporta la configurazione di fifree')
+                ->addArgument('entity', InputArgument::REQUIRED, 'Entity da esportare')
+                ->addOption('append', null, InputOption::VALUE_NONE, 'Append per export');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $fs = new Filesystem;
+        $append = $input->getOption('append');
+        $this->em = $this->getContainer()->get("doctrine")->getManager();
+        $this->output = $output;
+        $entity = $input->getArgument('entity');
+
+        try {
+            //$fixturefile = $this->getContainer()->get('kernel')->locateResource('@FiCoreBundle/Resources/config/fixtures.yml');
+            $fixturefile = dirname($this->getContainer()->get('kernel')->getRootDir()) . "/doc" . DIRECTORY_SEPARATOR . "fixtures.yml";
+            if (!$append) {
+                $fs->remove($fixturefile);
+            }
+            return $this->export($fixturefile, $entity);
+        } catch (\Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+
+    protected function export($fixturefile, $entity)
+    {
+        $entityclass = "Fi\\CoreBundle\\Entity\\" . $entity;
+        $ret = $this->exportEntity($fixturefile, $entityclass);
+        if ($ret == 0) {
+            foreach ($this->entities as $entity) {
+                $this->output->writeln("<info>Esporto " . $entity . " su file</info>");
+                $this->exportEntityToFile($fixturefile, $entity);
+            }
+            $this->exportEntityToFile($fixturefile, $entityclass);
+            return 0;
+        }
+        return 1;
+    }
+
+    private function exportEntity($fixturefile, $entityclass)
+    {
+
+        $this->output->writeln("<info>Export Entity: " . $entityclass . "</info>");
+        if ($this->entityExists($entityclass)) {
+            $hasEntityCollegate = $this->hasJoinTables($entityclass);
+            if ($hasEntityCollegate) {
+                $this->output->writeln("<info>Entity " . $entityclass . " ha tabelle in join</info>");
+                $entityCollegata = $this->entityJoinTables($entityclass);
+                foreach ($entityCollegata as $key => $tabella) {
+                    $this->entities[] = $key;
+                    $this->output->writeln("<info>Prima esporto " . $key . " per " . $tabella . "</info>");
+                    $this->exportEntity($fixturefile, $key);
+                }
+            }
+        } else {
+            $this->output->writeln("<error>Entity not found: " . $entityclass . " </error>");
+            return 1;
+        }
+        return 0;
+    }
+
+    private function exportEntityToFile($fixturefile, $entityclass)
+    {
+        $entityDump = array();
+
+
+        $query = $this->em->createQueryBuilder()
+                ->select('p')
+                ->from($entityclass, 'p')
+                ->getQuery()
+        ;
+        $repo = $query->getArrayResult();
+
+
+        //$repo = $this->em->getRepository($entityclass)->findAll();
+        $this->output->writeln("<info>Trovate " . count($repo) . " records per l'entity " . $entityclass . "</info>");
+        foreach ($repo as $row) {
+            $entityDump[$entityclass][] = $row;
+        }
+        $yml = Yaml::dump($entityDump);
+        file_put_contents($fixturefile, $yml, FILE_APPEND);
+    }
+
+    private function entityExists($className)
+    {
+
+        if (is_object($className)) {
+            $className = ($className instanceof Proxy) ? get_parent_class($className) : get_class($className);
+        }
+
+        return !$this->em->getMetadataFactory()->isTransient($className);
+    }
+
+    private function entityJoinTables($entityclass)
+    {
+        $jointables = array();
+        $metadata = $this->em->getClassMetadata($entityclass);
+        $fielsassoc = $metadata->associationMappings;
+        foreach ($fielsassoc as $tableassoc) {
+            if ($tableassoc["inversedBy"]) {
+                $jointables[$tableassoc["targetEntity"]] = array("entity" => $tableassoc);
+            }
+        }
+        return $jointables;
+    }
+
+    private function hasJoinTables($entityclass)
+    {
+        $jointables = $this->entityJoinTables($entityclass);
+        return count($jointables) > 0 ? true : false;
+    }
+}
---
+++ src/Fi/CoreBundle/Command/Fifree2configuratorimportCommand.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Fi\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Fifree2configuratorimportCommand extends ContainerAwareCommand
+{
+
+    private $forceupdate = false;
+    private $verboso = false;
+
+    protected function configure()
+    {
+        $this
+                ->setName('fifree2:configuratorimport')
+                ->setDescription('Configuratore per Fifree')
+                ->setHelp('Importa la configurazione di fifree da file fixtures.yml')
+                ->addOption('forceupdate', null, InputOption::VALUE_NONE, 'Forza update di record con id già presente')
+                ->addOption('verboso', null, InputOption::VALUE_NONE, 'Visualizza tutti i messaggi di importazione');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->em = $this->getContainer()->get("doctrine")->getManager();
+        $this->output = $output;
+        $this->forceupdate = $input->getOption('forceupdate');
+        $this->verboso = $input->getOption('verboso');
+
+        try {
+            //$fixturefile = $this->getContainer()->get('kernel')->locateResource('@FiCoreBundle/Resources/config/fixtures.yml');
+            $fixturefile = dirname($this->getContainer()->get('kernel')->getRootDir()) . "/doc" . DIRECTORY_SEPARATOR . "fixtures.yml";
+            return $this->import($fixturefile);
+        } catch (\Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+
+    protected function import($fixturefile)
+    {
+        $fs = new Filesystem;
+        if ($fs->exists($fixturefile)) {
+            $fixtures = Yaml::parse(file_get_contents($fixturefile));
+            $msg = "<info>Trovate " . count($fixtures) . " entities nel file " . $fixturefile . "</info>";
+            $this->output->writeln($msg);
+            foreach ($fixtures as $entityclass => $fixture) {
+                $this->executeImport($entityclass, $fixture);
+            }
+        }
+    }
+
+    private function executeImport($entityclass, $fixture)
+    {
+        $msg = "<info>Trovati " . count($fixture) . " record per l'entity " . $entityclass . "</info>";
+        $this->output->writeln($msg);
+        foreach ($fixture as $record) {
+            $objrecord = $this->em->getRepository($entityclass)->find($record["id"]);
+            if (count($objrecord) > 0) {
+                if ($this->forceupdate) {
+                    $retcode = $this->executeUpdate($entityclass, $record, $objrecord);
+                    if ($retcode !== 0) {
+                        return 1;
+                    }
+                } else {
+                    $msgerr = "<error>" . $entityclass . " con id " . $record["id"]
+                            . " non modificata, specificare l'opzione --forceupdate "
+                            . "per sovrascrivere record presenti</error>";
+                    $this->output->writeln($msgerr);
+                }
+            } else {
+                $retcode = $this->executeInsert($entityclass, $record);
+                if ($retcode !== 0) {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    private function executeInsert($entityclass, $record)
+    {
+        $objrecord = new $entityclass();
+
+        foreach ($record as $key => $value) {
+            if ($key !== 'id' && $value) {
+                $propertyEntity = $this->getFieldNameForEntity($key, $objrecord);
+                $setfieldname = $propertyEntity["set"];
+                $objrecord->$setfieldname($value);
+            }
+        }
+        /* @var $em \Doctrine\ORM\EntityManager */
+        $this->em->persist($objrecord);
+        
+        $this->em->flush();
+        $this->em->clear();
+        $infomsg = "<info>" . $entityclass . " con id " . $objrecord->getId() . " aggiunta</info>";
+        $this->output->writeln($infomsg);
+        if ($record["id"] !== $objrecord->getId()) {
+            try {
+                $qb = $this->em->createQueryBuilder();
+                $q = $qb->update($entityclass, 'u')
+                        ->set('u.id', ":newid")
+                        ->where('u.id = :oldid')
+                        ->setParameter("newid", $record["id"])
+                        ->setParameter("oldid", $objrecord->getId())
+                        ->getQuery();
+                $q->execute();
+                $msgok = "<info>" . $entityclass . " con id " . $objrecord->getId() . " sistemata</info>";
+                $this->output->writeln($msgok);
+            } catch (\Exception $exc) {
+                echo $exc->getMessage();
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    private function executeUpdate($entityclass, $record, $objrecord)
+    {
+
+        foreach ($record as $key => $value) {
+            if ($key !== 'id' && $value) {
+                $propertyEntity = $this->getFieldNameForEntity($key, $objrecord);
+                $getfieldname = $propertyEntity["get"];
+                $setfieldname = $propertyEntity["set"];
+                $cambiato = ($objrecord->$getfieldname() == $value);
+                if ($cambiato) {
+                    if ($this->verboso) {
+                        $msginfo = "<info>" . $entityclass . " con id " . $record["id"]
+                                . " per campo " . $key . " non modificato perchè già "
+                                . $value . "</info>";
+                        $this->output->writeln($msginfo);
+                    }
+                } else {
+                    try {
+                        if (is_array($value)) {
+                            $msgarray = "<info>" . $entityclass . " con id " . $record["id"]
+                                    . " per campo " . $key . " cambio valore da "
+                                    . json_encode($objrecord->$getfieldname()) . " a "
+                                    . json_encode($value) . " in formato array" . "</info>";
+                            $this->output->writeln($msgarray);
+                            $objrecord->$setfieldname($value);
+                        } else {
+                            $msgok = "<info>" . $entityclass . " con id " . $record["id"]
+                                    . " per campo " . $key . " cambio valore da " . $objrecord->$getfieldname()
+                                    . " a " . $value . "</info>";
+                            $this->output->writeln($msgok);
+                            $objrecord->$setfieldname($value);
+                        }
+                    } catch (\Exception $exc) {
+                        $msgerr = "<error>" . $entityclass . " con id " . $record["id"]
+                                . " per campo " . $key . ", ERRORE: " . $exc->getMessage()
+                                . " alla riga " . $exc->getLine() . "</error>";
+                        $this->output->writeln($msgerr);
+                        return 1;
+                    }
+                }
+            }
+        }
+
+        $this->em->persist($objrecord);
+        $this->em->flush();
+        return 0;
+    }
+
+    private function getFieldNameForEntity($fieldname, $objrecord)
+    {
+        $parametri = array('str' => $fieldname, 'primamaiuscola' => true);
+        $getfieldname = \Fi\CoreBundle\Utils\GrigliaUtils::toCamelCase($parametri);
+        if (!method_exists($objrecord, $getfieldname)) {
+            $getfieldname = "has" . \Fi\CoreBundle\Utils\GrigliaUtils::toCamelCase($parametri);
+            if (!method_exists($objrecord, $getfieldname)) {
+                $getfieldname = "is" . \Fi\CoreBundle\Utils\GrigliaUtils::toCamelCase($parametri);
+                if (!method_exists($objrecord, $getfieldname)) {
+                    $getfieldname = "get" . \Fi\CoreBundle\Utils\GrigliaUtils::toCamelCase($parametri);
+                }
+            }
+        }
+        $setfieldname = "set" . \Fi\CoreBundle\Utils\GrigliaUtils::toCamelCase($parametri);
+
+        return array("get" => $getfieldname, "set" => $setfieldname);
+    }
+}
