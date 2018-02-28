90b76bb5271e3f63fde1cc95811b6bfea35e5e62
d59495 <andrea.manzi@comune.fi.it>
2018-02-28 13:34:05 +0100

    add tests

--- tests/Fi/CoreBundle/Controller/MenuApplicazioneControllerTest.php
+++ tests/Fi/CoreBundle/Controller/MenuApplicazioneControllerTest.php
@@ -24,6 +24,72 @@ class MenuApplicazioneControllerTest extends FifreeTestAuthorizedClient
         $body = $crawler->filter('div[id="MenuApplicazione"]');
         $attributes = $body->extract(array('_text', 'class'));
         $this->assertEquals($attributes[0][1], 'tabella');
+
+
+//insert
+        $crawler = $client->request('GET', '/MenuApplicazione/new');
+        $this->assertTrue($crawler->filter('html:contains("formdatiMenuApplicazione")')->count() > 0);
+        $descrizione = "nome";
+        /* Inserimento */
+        if (version_compare(\Symfony\Component\HttpKernel\Kernel::VERSION, '3.0') >= 0) {
+            $fieldprefix = 'menu_applicazione';
+        } else {
+            $fieldprefix = 'fi_corebundle_menu_applicazionetype';
+        }
+        $valore = "provacrawler";
+        $campodescrizione = $fieldprefix . "[" . $descrizione . "]";
+        $form = $crawler->filter('form[id=formdatiMenuApplicazione]')->form(array("$campodescrizione" => $valore));
+
+        // submit that form
+        $crawler = $client->submit($form);
+
+        sleep(1);
+
+        //update
+        /* @var $qb \Doctrine\ORM\QueryBuilder */
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select(array('a'));
+        $qb->from('FiCoreBundle:MenuApplicazione', 'a');
+        $qb->where('a.nome = :descrizione');
+        $qb->setParameter('descrizione', $valore);
+        $record = $qb->getQuery()->getResult();
+        $recordadded = $record[0];
+
+        $this->assertEquals($recordadded->getNome(), $valore);
+
+        $crawler = $client->request('GET', '/MenuApplicazione/' . $recordadded->getId() . '/edit');
+        $this->assertTrue($crawler->filter('html:contains("formdatiMenuApplicazione")')->count() > 0);
+
+        $valorenew = "provacrawler2";
+        $campodescrizione = $fieldprefix . "[" . $descrizione . "]";
+        $form = $crawler->filter('form[id=formdatiMenuApplicazione]')->form(array("$campodescrizione" => $valorenew));
+
+        // submit that form
+        $crawler = $client->submit($form);
+
+
+        $em->clear();
+        $em = $this->getEntityManager();
+        $qb2 = $em->createQueryBuilder();
+        $qb2->select(array('a'));
+        $qb2->from('FiCoreBundle:MenuApplicazione', 'a');
+        $qb2->where('a.nome = :descrizione');
+        $qb2->setParameter('descrizione', $valorenew);
+        $record2 = $qb2->getQuery()->getResult();
+        $recorddelete = $record2[0];
+        $this->assertEquals($recorddelete->getNome(), $valorenew);
+
+        //delete
+        $crawler = $client->request('GET', '/MenuApplicazione/' . $recorddelete->getId() . '/edit');
+        $this->assertTrue($crawler->filter('html:contains("formdatiMenuApplicazione")')->count() > 0);
+        $btn = $crawler->selectLink("Elimina")->link();
+        $client->click($btn);
+
+        //Non si riesce a premere Cancella si vede perchè viene creato a runtime il pulsante
+        //quindi lancio la delete a mano
+
+        $crawler = $client->request('GET', '/MenuApplicazione/' . $recorddelete->getId() . '/delete');
     }
 
 }
--- tests/Fi/CoreBundle/Controller/OpzioniTabellaControllerTest.php
+++ tests/Fi/CoreBundle/Controller/OpzioniTabellaControllerTest.php
@@ -23,6 +23,72 @@ class OpzioniTabellaControllerTest extends FifreeTestAuthorizedClient
         $attributes = $body->extract(array('_text', 'class'));
         $this->assertEquals($attributes[0][1], 'tabella');
 
+
+//insert
+        $crawler = $client->request('GET', '/OpzioniTabella/new');
+        $this->assertTrue($crawler->filter('html:contains("formdatiOpzioniTabella")')->count() > 0);
+        $descrizione = "descrizione";
+        /* Inserimento */
+        if (version_compare(\Symfony\Component\HttpKernel\Kernel::VERSION, '3.0') >= 0) {
+            $fieldprefix = 'opzioni_tabella';
+        } else {
+            $fieldprefix = 'fi_corebundle_opzioni_tabellatype';
+        }
+        $valore = "provacrawler";
+        $campodescrizione = $fieldprefix . "[" . $descrizione . "]";
+        $campotabella= $fieldprefix . "[tabelle]";
+        $form = $crawler->filter('form[id=formdatiOpzioniTabella]')->form(array("$campodescrizione" => $valore,"$campotabella" => 1));
+
+        // submit that form
+        $crawler = $client->submit($form);
+
+        sleep(1);
+
+        //update
+        /* @var $qb \Doctrine\ORM\QueryBuilder */
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select(array('a'));
+        $qb->from('FiCoreBundle:OpzioniTabella', 'a');
+        $qb->where('a.descrizione = :descrizione');
+        $qb->setParameter('descrizione', $valore);
+        $record = $qb->getQuery()->getResult();
+        $recordadded = $record[0];
+
+        $this->assertEquals($recordadded->getDescrizione(), $valore);
+
+        $crawler = $client->request('GET', '/OpzioniTabella/' . $recordadded->getId() . '/edit');
+        $this->assertTrue($crawler->filter('html:contains("formdatiOpzioniTabella")')->count() > 0);
+
+        $valorenew = "provacrawler2";
+        $campodescrizione = $fieldprefix . "[" . $descrizione . "]";
+        $form = $crawler->filter('form[id=formdatiOpzioniTabella]')->form(array("$campodescrizione" => $valorenew));
+
+        // submit that form
+        $crawler = $client->submit($form);
+
+
+        $em->clear();
+        $em = $this->getEntityManager();
+        $qb2 = $em->createQueryBuilder();
+        $qb2->select(array('a'));
+        $qb2->from('FiCoreBundle:OpzioniTabella', 'a');
+        $qb2->where('a.descrizione = :descrizione');
+        $qb2->setParameter('descrizione', $valorenew);
+        $record2 = $qb2->getQuery()->getResult();
+        $recorddelete = $record2[0];
+        $this->assertEquals($recorddelete->getDescrizione(), $valorenew);
+
+        //delete
+        $crawler = $client->request('GET', '/OpzioniTabella/' . $recorddelete->getId() . '/edit');
+        $this->assertTrue($crawler->filter('html:contains("formdatiOpzioniTabella")')->count() > 0);
+        $btn = $crawler->selectLink("Elimina")->link();
+        $client->click($btn);
+
+        //Non si riesce a premere Cancella si vede perchè viene creato a runtime il pulsante
+        //quindi lancio la delete a mano
+
+        $crawler = $client->request('GET', '/OpzioniTabella/' . $recorddelete->getId() . '/delete');
     }
 
 }
