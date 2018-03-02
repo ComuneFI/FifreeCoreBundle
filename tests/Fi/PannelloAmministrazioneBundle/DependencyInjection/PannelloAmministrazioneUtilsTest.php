384acd13296231f7156fa1b83e17486888c29dda
Andrea Manzi <manzolo@libero.it>
2018-03-02 21:20:39 +0100

    add test clear cache

--- tests/Fi/PannelloAmministrazioneBundle/DependencyInjection/PannelloAmministrazioneUtilsTest.php
+++ tests/Fi/PannelloAmministrazioneBundle/DependencyInjection/PannelloAmministrazioneUtilsTest.php
@@ -8,9 +8,14 @@ class PannelloAmministrazioneUtilsTest extends FifreeTestAuthorizedClient
     public function testClearCache()
     {
         $client = $this->getClient();
+        $container = $client->getContainer();
+        $apppath = new \Fi\PannelloAmministrazioneBundle\DependencyInjection\ProjectPath($container);
+        $cachepath = $apppath->getCachePath() . "/prod";
 
-        $pa = new Fi\PannelloAmministrazioneBundle\DependencyInjection\PannelloAmministrazioneUtils($client->getContainer());
+
+        $pa = new Fi\PannelloAmministrazioneBundle\DependencyInjection\PannelloAmministrazioneUtils($container);
         $pa->clearcache("prod");
+        $this->assertTrue(file_exists($cachepath));
     }
 
 }
