25a6a31bf2a0b5f8fc6c835a9af38e562a864edc
Andrea Manzi <manzolo@libero.it>
2018-03-05 21:50:28 +0100

    exit unix command istaead response

--- src/Fi/PannelloAmministrazioneBundle/Controller/PannelloAmministrazioneController.php
+++ src/Fi/PannelloAmministrazioneBundle/Controller/PannelloAmministrazioneController.php
@@ -306,7 +306,7 @@ class PannelloAmministrazioneController extends Controller
     /**
      * Suppress PMD warnings per exit.
      *
-     * @//SuppressWarnings(PHPMD)
+     * @SuppressWarnings(PHPMD)
      */
     public function unixCommandAction(Request $request)
     {
@@ -333,8 +333,8 @@ class PannelloAmministrazioneController extends Controller
                         str_replace(';', '<br/>', str_replace('&&', '<br/>', $command)) .
                         '</i><br/><i style = "color: red;">' . nl2br($result['errmsg']) . '</i>';
 
-                return new Response($errmsg);
-                //exit(nl2br($errmsg));
+                //return new Response($errmsg);
+                exit(nl2br($errmsg));
                 //Uso exit perchè new response avendo cancellato la cache schianta non avendo più a disposizione i file
                 //return;
                 /* return new Response('Errore nel comando: <i style = "color: white;">' .
@@ -344,8 +344,8 @@ class PannelloAmministrazioneController extends Controller
                     str_replace(';', '<br/>', str_replace('&&', '<br/>', $command)) . '</i><br/>' .
                     nl2br($result['errmsg']) . '</pre>';
             //Uso exit perchè new response avendo cancellato la cache schianta non avendo più a disposizione i file
-            return new Response($msgok);
-            //exit(nl2br($msgok));
+            //return new Response($msgok);
+            exit(nl2br($msgok));
             //return;
             /* return new Response('<pre>Eseguito comando: <i style = "color: white;">' . $command .
              * '</i><br/>' . str_replace("\n", "<br/>", $process->getOutput()) . "</pre>"); */
