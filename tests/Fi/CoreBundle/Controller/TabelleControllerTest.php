b3a1869dfde4de7e3c1abf64130c6568d6f86fcc
Andrea Manzi <andrea.manzi@comune.fi.it>
2018-10-08 16:42:56 +0200

    add tests

--- tests/Fi/CoreBundle/Controller/TabelleControllerTest.php
+++ tests/Fi/CoreBundle/Controller/TabelleControllerTest.php
@@ -33,4 +33,42 @@ class TabelleControllerTest extends FifreeTestAuthorizedClient
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
+    public function testAggiornaTabelle()
+    {
+        $client = $this->getClient();
+        $url = $client->getContainer()->get('router')->generate('Tabelle_aggiorna', array(
+            "etichettaindex" => "",
+            "etichettastampa" => "",
+            "id" => "12",
+            "larghezzaindex" => "",
+            "larghezzastampa" => "",
+            "mostraindex" => 1,
+            "mostrastampa" => 1,
+            "oper" => "edit",
+            "ordineindex" => 10,
+            "ordinestampa" => "",
+            "registrastorico" => 0,
+        ));
+
+        $client->request('POST', $url);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        
+        $url2 = $client->getContainer()->get('router')->generate('Tabelle_aggiorna', array(
+            "etichettaindex" => "",
+            "etichettastampa" => "",
+            "id" => "12",
+            "larghezzaindex" => "",
+            "larghezzastampa" => "",
+            "mostraindex" => 1,
+            "mostrastampa" => 1,
+            "oper" => "edit",
+            "ordineindex" => "",
+            "ordinestampa" => "",
+            "registrastorico" => 0,
+        ));
+
+        $client->request('POST', $url2);
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
 }
