920a5feaeb8f9d9ae114e1cae6220ab23535c38f
Andrea Manzi <manzolo@libero.it>
2018-10-10 19:25:38 +0200

    Inizio 4.0

--- tests/config/bundles.php
+++ tests/config/bundles.php
@@ -1,12 +1,11 @@
 <?php
 
-$bundles = [
+return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
@@ -16,12 +15,5 @@ $bundles = [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Fi\CoreBundle\FiCoreBundle::class => ['all' => true],
-    Fi\PannelloAmministrazioneBundle\PannelloAmministrazioneBundle::class => ['all' => true]
+    Fi\PannelloAmministrazioneBundle\PannelloAmministrazioneBundle::class => ['all' => true],
 ];
-$currentDir = dirname(dirname(__FILE__)) . '/../';
-
-if (file_exists($currentDir . 'src' . DIRECTORY_SEPARATOR . 'Fi' . DIRECTORY_SEPARATOR . 'ProvaBundle')) {
-    $bundles[Fi\ProvaBundle\FiProvaBundle::class] = ['test' => true];
-}
-
-return $bundles;
